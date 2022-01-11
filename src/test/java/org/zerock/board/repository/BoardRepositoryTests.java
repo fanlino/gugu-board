808ed6333f76e696a9053d3d989d9f509600823e
D.O <d.o@kakaoenterrpise.com>
2022-01-11 21:54:29 +0900

    join 테스트

--- src/test/java/org/zerock/board/repository/BoardRepositoryTests.java
+++ src/test/java/org/zerock/board/repository/BoardRepositoryTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.zerock.board.entity.Board;
 import org.zerock.board.entity.Member;
 
+import java.util.Optional;
 import java.util.stream.IntStream;
 
 @SpringBootTest
@@ -24,5 +25,15 @@ public class BoardRepositoryTests {
                     .build();
             boardRepository.save(board);
         });
+    }
+
+    @Test
+    public void testRead1() {
+        Optional<Board> result = boardRepository.findById(100L);
+
+        Board board = result.get();
+
+        System.out.println(board);
+        System.out.println(board.getWriter());
     }
 }
